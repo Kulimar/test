3db34f585cf11d2120e48908dc6fbfba3ddc6c83
Kulimar <159603128+Kulimar@users.noreply.github.com>
2025-05-19 13:25:10 -0700

    Fix 404 on images list route

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.get('/images-list', (req, res) => {
   const dir = path.join(__dirname, 'images');
   fs.readdir(dir, (err, files) => {
@@ -20,6 +14,12 @@ app.get('/images-list', (req, res) => {
     const imageFiles = files.filter(file => /\.(jpe?g|png|gif|bmp|webp|heic)$/i.test(file));
     res.json(imageFiles);
   });
+});
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
